f27fd359b1cd5e47ddde87c60c31096bf291a7fe
Pankaj <pankajchaudhary0403@gmail.com>
2026-08-23 14:33:18 +0530

    Database url changes

--- Server/DataBase/Connection.py
+++ Server/DataBase/Connection.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base,sessionmaker
-DATABASE_URL = "postgresql+psycopg://postgres:Pankaj0403%40@localhost/Rapido"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABSE_URL=os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
