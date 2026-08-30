7b0a06a3a1e35063d62d1864c2fa820f6f36ddc6
Pankaj <pankajchaudhary0403@gmail.com>
2026-08-30 16:35:39 +0530

    Request function

--- Server/routes/driver_routes.py
+++ Server/routes/driver_routes.py
@@ -191,7 +191,7 @@ def get_profile(driver_id:int):
     rider=result["driver"]
     return{
         "status":True,
-        "id":rider.id,
+        "driver_id":rider.id,
         "name":rider.name,
         "surname":rider.surname,
         "rating":rider.rating,
