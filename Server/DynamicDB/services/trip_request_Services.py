472ab5f650209d8d1258941891992cb9ef4d7067
Pankaj <pankajchaudhary0403@gmail.com>
2026-09-01 00:48:49 +0530

    Request function

--- Server/DynamicDB/services/trip_request_Services.py
+++ Server/DynamicDB/services/trip_request_Services.py
@@ -1,10 +1,14 @@
 from DynamicDB.models.triprequest import TripRequest
 from DynamicDB.repositories.Trip_request_repo import TripRequestRepository
 class TripRequestServices:
-    def registerRequest(self,driver_id,user_id):
+    def registerRequest(self,driver_id,user_id,pickup_lat,pickup_long,dest_lat,dest_long):
         request=TripRequest(
             driver_id=driver_id,
-            user_id=user_id
+            user_id=user_id,
+            pickup_lat=pickup_lat,
+            pickup_long=pickup_long,
+            dest_lat=dest_lat,
+            dest_long=dest_long
         )
         TripRequestRepository.add(request)
         return request
