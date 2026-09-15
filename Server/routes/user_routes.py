71e3804d30176ded7403efa65aa26312e5e16555
Pankaj <pankajchaudhary0403@gmail.com>
2026-09-15 18:49:50 +0530

    Websocket sync issue

--- Server/routes/user_routes.py
+++ Server/routes/user_routes.py
@@ -207,7 +207,7 @@ def me(user_id:int=Depends(get_current_user)):
 
 trip_request=TripRequestServices()
 @router.patch("/driver-response")
-def gte_driver_response(trip_id:int,user_id:int=Depends(get_current_user)):
+def get_driver_response(trip_id:int,user_id:int=Depends(get_current_user)):
     response=trip_request.get_driver_response(trip_id,user_id)
     return{
         "status":response.status
