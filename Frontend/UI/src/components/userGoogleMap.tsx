acc8a12f9f9781e787779f5cbad2212472ac9ebd
Pankaj <pankajchaudhary0403@gmail.com>
2026-09-18 14:26:54 +0530

    Header update

--- Frontend/UI/src/components/userGoogleMap.tsx
+++ Frontend/UI/src/components/userGoogleMap.tsx
@@ -362,7 +362,7 @@ function UserGoogleMap({location,drivers,onDriverSelect,selectedDriver,onCloseDr
                     <div 
                     style={{
                         position: "absolute",
-                        bottom: "200px",
+                        bottom: "500px",
                         right: "20px",
                         zIndex: 20,
                     }}>
@@ -389,7 +389,7 @@ function UserGoogleMap({location,drivers,onDriverSelect,selectedDriver,onCloseDr
                     <div
                     style={{
                         position: "absolute",
-                        bottom: "100px",
+                        bottom: "150px",
                         left: "50%",
                         transform:"translateX(-50%)",
                         zIndex: 20,
