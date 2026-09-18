daa98234efc19dd403227c74bf3c666c034f4abf
Pankaj <pankajchaudhary0403@gmail.com>
2026-09-18 12:36:28 +0530

    Header update

--- Frontend/UI/src/pages/Drivers/driverMaps.tsx
+++ Frontend/UI/src/pages/Drivers/driverMaps.tsx
@@ -145,7 +145,8 @@ const buttonStyle = {
             style={{
                 width: "100%",
                 height: "100vh",
-                backgroundColor: "#F8FAFC"
+                backgroundColor: "#F8FAFC",
+                flexDirection: "column"
             }}
         >
             {/* Top Toolbar */}
@@ -242,7 +243,7 @@ const buttonStyle = {
                 </button>
 
             </div>
-            <div style={{height:"100vh",border:"5px solid #2563EB",borderRadius:"12px",overflow: "hidden",boxShadow: "0 4px 12px rgba(37, 99, 235, 0.2)"}}>
+            <div style={{flex:1,height:"100vh",border:"5px solid #2563EB",borderRadius:"12px",overflow: "hidden",boxShadow: "0 4px 12px rgba(37, 99, 235, 0.2)"}}>
                 <DriverGoogleMap 
                 location={location} 
                 requests={requests}
